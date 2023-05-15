7aa5ccdff8b82f7328c698043528e98f811a5a47
omid-j-d <69404929+omid-j-d@users.noreply.github.com>
2023-05-15 21:22:04 +0330

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 read -p "Enter domain name: " domain
-
+read -p "Enter your email: " email
 apt install curl socat -y
 
 curl https://get.acme.sh | sh
 
 ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 
-~/.acme.sh/acme.sh --register-account -m omid7975790@gmail.com
+~/.acme.sh/acme.sh --register-account -m $email
 
 ~/.acme.sh/acme.sh --issue -d $domain --standalone
 
